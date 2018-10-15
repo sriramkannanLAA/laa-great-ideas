57a2a681131bd66b4492dddd2b003750a01f92b7
David Elliott <david.elliott@justice.gov.uk>
2018-10-15 13:37:46 +0100

    Remove coffee script and replace with javascript

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module GreatIdeas
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+# Set default javascript
+    config.generators.javascript_engine = :js
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
