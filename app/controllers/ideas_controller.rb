34571c5ad1cb4e45dc463868abc439d5bdfc8a3c
Katherine Ahern <katharine.ahern@legalaid.gsi.gov.uk>
2018-10-15 14:37:09 +0100

    WIP - Fixing scaffolded tests

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -24,7 +24,7 @@ class IdeasController < ApplicationController
   # POST /ideas
   # POST /ideas.json
   def create
-    @idea = Idea.new(idea_params)
+    @idea = current_user.ideas.new(idea_params)
 
     respond_to do |format|
       if @idea.save
