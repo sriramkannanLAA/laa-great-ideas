a4341e01eed505df91b9cd5ec00c474c775bd4eb
David Elliott <david.elliott@justice.gov.uk>
2018-10-16 10:00:51 +0100

    Add enums for benefits, involvement and it systems

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,4 +1,37 @@
 class Idea < ApplicationRecord
   belongs_to :user
   validates :title, presence: true
+  
+  enum benefits: [
+    :better_decision_making,
+    :improved_reputation,
+    :reduced_risk,
+    :time_saved,
+    :cost,
+    :improved_service,
+    :staff_engagement_and_moral
+  ]
+
+  enum it_system: [
+    :cclf,
+    :ccms,
+    :ccr,
+    :cis,
+    :cwa,
+    :eforms,
+    :laa_online_portal,
+    :maat,
+    :maat_libra,
+    :management_information,
+    :obiee,
+    :pims,
+    :tv
+  ]
+
+  enum involvement: [
+    :i_want_to_be_informed,
+    :i_want_to_be_involved,
+    :i_want_to_lead_on_this,
+    :no_involvement
+  ]
 end
