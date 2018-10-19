e882c03b22e07c4758901070f1583b9737ae1bb0
David Elliott <david.elliott@justice.gov.uk>
2018-10-19 15:26:26 +0100

    Simplify double

--- spec/helpers/ideas_helper_spec.rb
+++ spec/helpers/ideas_helper_spec.rb
@@ -4,12 +4,8 @@ RSpec.describe 'Ideas helper' do
   include IdeasHelper
   describe 'Enum to select' do
     it 'should return an array of keys and humanized values' do
-      enum1 = double('enum')
-      allow(enum1).to receive(:fetch).and_return(
-        'exceptional_and_complex_cases' => 0,
-        'crime' => 1
-      )
-      expect(enum_to_select(enum1.fetch)).to eq(
+      enum = double('enum', keys: ['exceptional_and_complex_cases', 'crime'])
+      expect(enum_to_select(enum)).to eq(
         [
           ['Exceptional and complex cases', 'exceptional_and_complex_cases'],
           ['Crime', 'crime']
