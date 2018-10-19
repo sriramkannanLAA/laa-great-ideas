7c21203f5715887f2f58e65036b825466fda0f1c
David Elliott <david.elliott@justice.gov.uk>
2018-10-19 13:24:15 +0100

    Remove dependancy on idea model with double

--- spec/helpers/ideas_helper_spec.rb
+++ spec/helpers/ideas_helper_spec.rb
@@ -1,11 +1,20 @@
 require 'rails_helper'
-include IdeasHelper
 
-RSpec.describe "Ideas helper" do
-  describe "Enum to select" do
-    it "should return an array of keys and humanized values" do
-      area = enum_to_select(Idea.business_areas).select{ |h, k| k == "exceptional_and_complex_cases"}
-      expect(area[0][0]).to eq("Exceptional and complex cases")
+RSpec.describe 'Ideas helper' do
+  include IdeasHelper
+  describe 'Enum to select' do
+    it 'should return an array of keys and humanized values' do
+      enum1 = double('enum')
+      allow(enum1).to receive(:fetch).and_return(
+        'exceptional_and_complex_cases' => 0,
+        'crime' => 1
+      )
+      expect(enum_to_select(enum1.fetch)).to eq(
+        [
+          ['Exceptional and complex cases', 'exceptional_and_complex_cases'],
+          ['Crime', 'crime']
+        ]
+      )
     end
   end
 end
