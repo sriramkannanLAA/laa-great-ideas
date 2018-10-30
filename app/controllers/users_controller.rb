27dfd26a26d9d1933aa04784b6e28eff31e0ac46
David Elliott <david.elliott@justice.gov.uk>
2018-10-30 17:12:14 +0000

    GI-14 As an admin manage users
    As an admin be able to add or remove other admin users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,21 @@
 class UsersController < ApplicationController
   before_action :require_admin
-  before_action :set_user, only: [:show]
+  before_action :set_user, only: [:show, :toggle_admin]
 
   def index
     @users = User.all
   end
 
   def show
+  end
+
+  def toggle_admin
+    @user.admin = !@user.admin
+    if @user.save
+      redirect_to user_path(@user), notice: 'Admin status updated'
+    else
+      redirect_to user_path(@user), notice: 'Unable to update admin status'
+    end
   end
 
   private
