acbf06ca4eb91b8aece7f43009e05d1c8e89170f
Katherine Ahern <katharine.ahern@legalaid.gsi.gov.uk>
2018-11-08 14:20:27 +0000

    Fixed typo as requested in pr

--- db/migrate/20181105133919_add_assigned_user_to_ideas.rb
+++ db/migrate/20181105133919_add_assigned_user_to_ideas.rb
@@ -1,5 +1,5 @@
 class AddAssignedUserToIdeas < ActiveRecord::Migration[5.2]
   def change
-    add_reference :ideas, :assigned_user, foreign_key: { to_table: :userts}
+    add_reference :ideas, :assigned_user, foreign_key: { to_table: :users}
   end
 end
