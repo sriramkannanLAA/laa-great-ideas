510d1516f263e78ed8db3bec7aa191122e015ade
David Elliott <david.elliott@justice.gov.uk>
2018-11-16 13:59:40 +0000

    GI-57 Add log out link
    Log out link added to top of all pages if the current user is logged in.
    This should be moved later to a better position eg nav bar when we have
    one.
    
    Also tidied up the urls for the users/sign_in etc to just /sign_in as
    the user part at the start is a little untidy.

---
+++ spec/system/sign_out_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'Sign out', type: :system do
+  before do
+    @user = User.create!(email: 'me@justice.gov.uk', password: 'change_me')
+  end
+
+  describe 'as a logged in user' do
+    it 'the sign out link should sign the user out' do
+      sign_in @user
+      visit ideas_path
+      click_link 'Sign out'
+      expect(page).to have_content 'Log in'
+    end
+  end
+
+  describe 'as a logged out user' do
+    it 'should not show a log out link' do
+      visit ideas_path
+      expect(page).not_to have_link('Sign out')
+    end
+  end
+end
